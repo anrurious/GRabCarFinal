46c1a280b612ffc313775742ad01ec1f03701a2e
ankured182 <ankured182@yahoo.com>
2016-05-10 14:46:47 +0600

    New Commit From 182
    New Commit From 182

--- GrabCarMVC/Controllers/DriverController.cs
+++ GrabCarMVC/Controllers/DriverController.cs
@@ -10,114 +10,107 @@ using GrabCarMVC.Models;
 
 namespace GrabCarMVC.Controllers
 {
-    public class DitclTestController : Controller
+    public class DriverController : Controller
     {
         private deshConnection db = new deshConnection();
 
-        // GET: /DitclTest/
+        // GET: /Driver/
         public ActionResult Index()
         {
-            return View(db.ditcltests.ToList());
+            return View(db.user_driver.ToList());
         }
 
-        [HttpPost]
-        public ActionResult Index(FormCollection fc, HttpPostedFileBase file)
-        {
-
-            return View();
-        }
-
-        // GET: /DitclTest/Details/5
-        public ActionResult Details(decimal id)
+        // GET: /Driver/Details/5
+        public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ditcltest ditcltest = db.ditcltests.Find(id);
-            if (ditcltest == null)
+            user_driver user_driver = db.user_driver.Find(id);
+            if (user_driver == null)
             {
                 return HttpNotFound();
             }
-            return View(ditcltest);
+            return View(user_driver);
         }
 
-        // GET: /DitclTest/Create
+        // GET: /Driver/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: /DitclTest/Create
+        // POST: /Driver/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include="Id,name")] ditcltest ditcltest)
+        public ActionResult Create([Bind(Include="id,first_name,last_name,fullname,phone,email,picture,bio,address,city,state,country,zipcode,dog_id,password,token,token_expiry,device_token,device_type,login_by,social_unique_id,created_at,updated_at,latitude,longitude,debt,deleted_at,rate,rate_count,promo_count,is_referee,user_type,gcm_reg_id,current_location_road,car_no,car_model_no,manufacture_year,car_type,gender,age,present_address,license_image,nid_image,brtc_image,insurance_image,status")] user_driver user_driver)
         {
             if (ModelState.IsValid)
             {
-                db.ditcltests.Add(ditcltest);
+                db.user_driver.Add(user_driver);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(ditcltest);
+            return View(user_driver);
         }
 
-        // GET: /DitclTest/Edit/5
-        public ActionResult Edit(decimal id)
+        // GET: /Driver/Edit/5
+        public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ditcltest ditcltest = db.ditcltests.Find(id);
-            if (ditcltest == null)
+            user_driver user_driver = db.user_driver.Find(id);
+            if (user_driver == null)
             {
                 return HttpNotFound();
             }
-            return View(ditcltest);
+            return View(user_driver);
         }
 
-        // POST: /DitclTest/Edit/5
+        // POST: /Driver/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="Id,name")] ditcltest ditcltest)
+        public ActionResult Edit([Bind(Include="id,first_name,last_name,fullname,phone,email,picture,bio,address,city,state,country,zipcode,dog_id,password,token,token_expiry,device_token,device_type,login_by,social_unique_id,created_at,updated_at,latitude,longitude,debt,deleted_at,rate,rate_count,promo_count,is_referee,user_type,gcm_reg_id,current_location_road,car_no,car_model_no,manufacture_year,car_type,gender,age,present_address,license_image,nid_image,brtc_image,insurance_image,status")] user_driver user_driver)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(ditcltest).State = EntityState.Modified;
+                db.Entry(user_driver).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(ditcltest);
+            return View(user_driver);
         }
 
-        // GET: /DitclTest/Delete/5
-        public ActionResult Delete(decimal id)
+        // GET: /Driver/Delete/5
+        public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ditcltest ditcltest = db.ditcltests.Find(id);
-            if (ditcltest == null)
+            user_driver user_driver = db.user_driver.Find(id);
+            if (user_driver == null)
             {
                 return HttpNotFound();
             }
-            return View(ditcltest);
+            return View(user_driver);
         }
 
-        // POST: /DitclTest/Delete/5
+        // POST: /Driver/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(decimal id)
+        public ActionResult DeleteConfirmed(int id)
         {
-            ditcltest ditcltest = db.ditcltests.Find(id);
-            db.ditcltests.Remove(ditcltest);
+            user_driver user_driver = db.user_driver.Find(id);
+            db.user_driver.Remove(user_driver);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
